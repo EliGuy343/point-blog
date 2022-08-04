c509344590672d73b688db9062b10eb06de3d6d5
Eli Guy <eliguy343@gmail.com>
2022-08-04 19:56:32 +0300

    Backend: signup user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,5 @@
 import User from '../models/User.js';
-
+import bcrypt from 'bcryptjs';
 export const getAllUsers = async (req,res) =>{
   let users;
   try {
@@ -10,3 +10,25 @@ export const getAllUsers = async (req,res) =>{
     console.log(err);
   }
 } 
+
+export const signupUser = async (req, res) => {
+  const {name, email, password} = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({email});
+    if(existingUser) {
+      return res.status(400).json({message:'email already in use'});
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword
+    });
+    await user.save();
+    res.status(200).json({user});
+  } 
+  catch (err) {
+    console.log(err);
+  }
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import {getAllUsers} from '../controllers/user.controller.js';
+import {getAllUsers, signupUser} from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get('/', getAllUsers);
+router.post('/signup', signupUser);
 export default router;
